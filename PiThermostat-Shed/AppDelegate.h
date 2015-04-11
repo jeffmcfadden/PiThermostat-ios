15c0f752d4ae6763167c2ba314f04c4f9a40bf04
jeffmcfadden <jeff@forgeapps.com>
2015-04-11 16:02:07 -0700

    Initial Commit

---
+++ PiThermostat-Shed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PiThermostat-Shed
+//
+//  Created by Jeff McFadden on 4/11/15.
+//  Copyright (c) 2015 ForgeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PiThermostat-Shed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PiThermostat-Shed
+//
+//  Created by Jeff McFadden on 4/11/15.
+//  Copyright (c) 2015 ForgeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
