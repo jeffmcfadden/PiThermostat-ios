dd503fe328016c9f7c20ce6b4fd3946a8fc186d9
jeffmcfadden <jeff@forgeapps.com>
2015-05-30 13:29:33 -0700

    Add license

---
+++ PiThermostat/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  PiThermostat
+//
+//  Created by Jeff McFadden on 5/30/15.
+//  Copyright (c) 2015 ForgeApps. All rights reserved.
+//
+
+#ifndef PiThermostat_Constants_h
+#define PiThermostat_Constants_h
+
+
+#endif
---
+++ PiThermostat/Constants.m
@@ -0,0 +1,9 @@
+//
+//  Constants.m
+//  PiThermostat
+//
+//  Created by Jeff McFadden on 5/30/15.
+//  Copyright (c) 2015 ForgeApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
